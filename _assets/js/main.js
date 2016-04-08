42206ed906a3949d603b901614da2af674ad2287
Brian Kelley <Brian.Kelley@jamfsoftware.com>
2016-04-08 18:38:39 -0500

    Update with new prism syntax highlighter

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -2,6 +2,8 @@
 //=require ../../bower_components/classie/classie.js
 //=require ../../bower_components/simple-jekyll-search/dest/jekyll-search.js
 //=require ../../bower_components/swipebox/src/js/jquery.swipebox.min.js
+//=require ../../bower_components/prism/prism.js
+//=require ../../bower_components/prism/components/prism-bash.js
 
 //=require scripts/_modal.js
 //=require scripts/_simple-jekyll-search.js
